21b561b9bd6a0d7601b7e817b2a045700e2e5a1e
a.vincelli <a.vincelli@d2454d56-e4fc-11de-9298-fbd6a473cdd1>
2010-02-04 20:59:47 +0000

    fixed url fallback

--- src/main/java/it/av/youeat/web/panel/FacebookLoginPanel.java
+++ src/main/java/it/av/youeat/web/panel/FacebookLoginPanel.java
@@ -52,7 +52,7 @@ public class FacebookLoginPanel extends Panel {
 
                 @Override
                 public void onClick(AjaxRequestTarget target) {
-                    getRequestCycle().setRequestTarget(new RedirectRequestTarget("http://www.facebook.com/login.php?api_key=083c31f005625c34a27aa011a279322b&extern=1&fbconnect=1&req_perms=publish_stream&return_session=1&v=1.0&next=http://www.youeat.org:8080/signIn"));
+                    getRequestCycle().setRequestTarget(new RedirectRequestTarget("http://www.facebook.com/login.php?api_key=083c31f005625c34a27aa011a279322b&extern=1&fbconnect=1&req_perms=publish_stream&return_session=1&v=1.0&next=http://www.youeat.org/signIn"));
                 }
                 
             });

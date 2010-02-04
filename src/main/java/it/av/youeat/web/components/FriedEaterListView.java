2086aadd46bf72ac81a32eaa358059727b605876
a.vincelli <a.vincelli@d2454d56-e4fc-11de-9298-fbd6a473cdd1>
2010-02-04 21:30:55 +0000

    fixed some links

--- src/main/java/it/av/youeat/web/components/FriedEaterListView.java
+++ src/main/java/it/av/youeat/web/components/FriedEaterListView.java
@@ -8,10 +8,9 @@ import it.av.youeat.web.page.YoueatHttpParams;
 import java.util.List;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.injection.web.InjectorHolder;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -19,7 +18,7 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 public class FriedEaterListView extends PropertyListView<ActivityRistorante> {
     @SpringBean
     private PeriodUtil periodUtil;
-    
+
     /**
      * Generate a list of friend that eat in the given risto
      * 
@@ -34,14 +33,8 @@ public class FriedEaterListView extends PropertyListView<ActivityRistorante> {
     @Override
     protected void populateItem(final ListItem<ActivityRistorante> item) {
         item.add(new Label("date.time", periodUtil.getPeriod(item.getModelObject().getDate().getTime(), getLocale())));
-        AjaxFallbackLink<String> eater = new AjaxFallbackLink<String>("view-eater") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                PageParameters pp = new PageParameters(YoueatHttpParams.YOUEAT_ID + "="
-                        + item.getModelObject().getEater().getId());
-                setResponsePage(EaterViewPage.class, pp);
-            }
-        };
+        BookmarkablePageLink eater = new BookmarkablePageLink("view-eater", EaterViewPage.class, new PageParameters(
+                YoueatHttpParams.YOUEAT_ID + "=" + item.getModelObject().getEater().getId()));
         eater.add(new Label("eater.firstname"));
         eater.add(new Label("eater.lastname"));
         item.add(eater);

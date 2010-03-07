9eaa4cced0e8c18b9152b387f20ce3794a4fb4b6
a.vincelli <a.vincelli@d2454d56-e4fc-11de-9298-fbd6a473cdd1>
2010-03-07 15:18:55 +0000

    fixed script

--- setup/script_1.0_to_1.1.sql
+++ setup/script_1.0_to_1.1.sql
@@ -1,19 +1,18 @@
-ALTER TABLE activity_ristorante ADD COLUMN ristorante_fk character varying(36) NOT NULL;
-
+ALTER TABLE activity_ristorante ADD COLUMN ristorante_fk character varying(36);
+--
 update activity_ristorante  set ristorante_fk  = ristorante ;
+--
 ALTER TABLE activity_ristorante ALTER COLUMN ristorante_fk SET NOT NULL;
 
-
---ALTER TABLE activity_ristorante DROP COLUMN ristorante;
--- set to not null ristorante_fk
-
+--
+ALTER TABLE activity_ristorante DROP COLUMN ristorante;
 
 alter table activity_ristorante 
         add constraint FK93343C9F70781DD 
         foreign key (ristorante_fk) 
         references ristorante;
 
-ALTER TABLE activity_ristorante DROP CONSTRAINT fk93343c9aa76a44b;
+--ALTER TABLE activity_ristorante DROP CONSTRAINT fk93343c9aa76a44b;
 
 --ALTER TABLE activity_ristorante DROP CONSTRAINT FK709D5753CC602F0E;
 --ALTER TABLE activity_ristorante DROP CONSTRAINT FK709D5753AA76A44B;

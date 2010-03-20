b1bd17159cb33d17841641fe3923cc88441fe1c6
a.vincelli <a.vincelli@d2454d56-e4fc-11de-9298-fbd6a473cdd1>
2010-03-20 09:32:58 +0000

    reformatted code

--- src/main/java/it/av/youeat/web/util/HtmlUtil.java
+++ src/main/java/it/av/youeat/web/util/HtmlUtil.java
@@ -15,8 +15,9 @@ import org.apache.wicket.model.IModel;
  * 
  */
 public final class HtmlUtil {
-    
-    private HtmlUtil(){};
+
+    private HtmlUtil() {
+    };
 
     public static final void fixInitialHtml(final Page page) {
         TransparentWebMarkupContainer html = new TransparentWebMarkupContainer("html");
